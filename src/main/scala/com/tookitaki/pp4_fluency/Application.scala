c77729ef3a9a928aa52e0796cc0fc7af591c80bb
Pavel Mukhin <pauri.lentonen@gmail.com>
2019-07-10 15:58:10 +0800

    Fixed pp4

--- src/main/scala/com/tookitaki/pp4_fluency/Application.scala
+++ src/main/scala/com/tookitaki/pp4_fluency/Application.scala
@@ -2,14 +2,13 @@ package com.tookitaki.pp4_fluency
 
 import cats.effect.IO
 import com.tookitaki.HikariConfig
-import com.tookitaki.util._
 import com.zaxxer.hikari.HikariDataSource
 import doobie.hikari.HikariTransactor
 
 import scala.concurrent.ExecutionContext
 
 object Application extends App {
-  implicit val ec = ExecutionContext.fromExecutor(exec)
+  implicit val ec = ExecutionContext.fromExecutor(com.tookitaki.util.exec)
   implicit val cs = IO.contextShift(ec)
 
   val config = HikariConfig(
@@ -23,6 +22,7 @@ object Application extends App {
     HikariTransactor.apply[IO](new HikariDataSource(config), ec, ec)
 
   val productRepository = new DoobieFluentProductRepository[IO](xa)
+  val putStrLn          = com.tookitaki.util.putStrLn[IO] _
 
   import cats.syntax.all._
 
@@ -32,11 +32,8 @@ object Application extends App {
     prod3      <- productRepository.findById(4)
     prod2      <- prod2Fiber.join
     prod1      <- prod1Fiber.join
-    _          <- putStrLn[IO]((prod1, prod2, prod3))
-  } yield
-    prod3.map { p =>
-      productRepository.update(p) *> IO()
-    }
+    _          <- putStrLn((prod1, prod2, prod3))
+  } yield ()
 
   ioProgram.unsafeRunSync()
 }
